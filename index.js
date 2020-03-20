14d28b16397bb4e11c3acf8defa810fab680301d
Mackenzie Davidson <mwvdavidson@gmail.com>
2020-03-20 15:00:40 -0400

    writing page

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ const cdn = process.env.cdn
 // here we declare our site config as cache - this is loaded if there any errors are thrown when trying to get our channel from are.na
 let cache
 if (environment === 'now') {
-	cache = yaml.safeLoad(fs.readFileSync(`${__dirname}/api/config.yaml`, 'utf8'))
-	// now requires `__dirname + ` vs the simpler `./*` for paths here. ref: https://github.com/zeit/ncc/issues/216 (says fixed but is not)
+  cache = yaml.safeLoad(fs.readFileSync(`${__dirname}/api/config.yaml`, 'utf8'))
+  // now requires `__dirname + ` vs the simpler `./*` for paths here. ref: https://github.com/zeit/ncc/issues/216 (says fixed but is not)
 } else {
-	cache = yaml.safeLoad(fs.readFileSync('./api/config.yaml', 'utf8'))
+  cache = yaml.safeLoad(fs.readFileSync('./api/config.yaml', 'utf8'))
 }
 const channelCache = require('./api/blog.json')
 
@@ -25,11 +25,11 @@ const app = express()
 app.use(compression())
 
 if (environment !== 'now') {
-	app.use('/js', express.static('js'))
-	app.use('/stylesheets', express.static('stylesheets'))
-	app.use('/fonts', express.static('fonts'))
-	app.use('/assets', express.static('assets'))
-	app.use('/api', express.static('api'))
+  app.use('/js', express.static('js'))
+  app.use('/stylesheets', express.static('stylesheets'))
+  app.use('/fonts', express.static('fonts'))
+  app.use('/assets', express.static('assets'))
+  app.use('/api', express.static('api'))
 }
 
 app.engine('html', mustacheExpress())
@@ -37,73 +37,120 @@ app.set('view engine', 'html')
 app.set('views', `${__dirname}/views`)
 
 app.get('/', async function(req, res) {
-	const view = req.query.view
-	const arena = new Arena({ accessToken: process.env.arenaPAT })
-	arena
-		.channel(process.env.arenaChannel)
-		.get({
-			page: 1, // get the first page of results
-			per: 64, // get 64 items per call (max: 100) - play around w this for performance
-			direction: 'desc' // ask API v nicely to sort blocks by most recent
-		})
-		.then(channel => {
-			// fetch our whole are.na channel as `channel`
+  const view = req.query.view
+  const arena = new Arena({ accessToken: process.env.arenaPAT })
+  arena
+    .channel(process.env.arenaChannel)
+    .get({
+      page: 1, // get the first page of results
+      per: 64, // get 64 items per call (max: 100) - play around w this for performance
+      direction: 'desc' // ask API v nicely to sort blocks by most recent
+    })
+    .then(channel => {
+      // fetch our whole are.na channel as `channel`
 
-			const config = yaml.safeLoad(channel.metadata.description) // get our site description from our are.na channel description - since it is loaded in as yaml, we can access it's values with `config.key`, ex: for title, we can use `config.details.title`
-			const contents = channel.contents // clean up the results a little bit, and make the channel's contents available as a constant, `contents`
+      const config = yaml.safeLoad(channel.metadata.description) // get our site description from our are.na channel description - since it is loaded in as yaml, we can access it's values with `config.key`, ex: for title, we can use `config.details.title`
+      const contents = channel.contents // clean up the results a little bit, and make the channel's contents available as a constant, `contents`
 
-			const calendar = contents.pop() // pop last block (in this case, "calendar"), out of array, and then pass it to the render below
+      const calendar = contents.pop() // pop last block (in this case, "calendar"), out of array, and then pass it to the render below
 
-			if (view === 'channel') {
-				// append `?view=channel` to the end of your URL to see "channel" as JSON
-				res.send(contents)
-			} else if (view === 'calendar') {
-				// append `?view=calendar` to the end of your URL to see "calendar" as JSON
-				res.send(calendar)
-			} else {
-				res.render('index.html', {
-					static_url: cdn,
-					config,
-					calendar,
-					arena: contents // pass our are.na channel contents into the render for use w mustache.js by using `{{arena}}` - see views/arena.html
-				})
-			}
-		})
-		.catch(err => {
-			// handle errors
+      if (view === 'channel') {
+        // append `?view=channel` to the end of your URL to see "channel" as JSON
+        res.send(contents)
+      } else if (view === 'calendar') {
+        // append `?view=calendar` to the end of your URL to see "calendar" as JSON
+        res.send(calendar)
+      } else {
+        res.render('index.html', {
+          static_url: cdn,
+          config,
+          calendar,
+          arena: contents // pass our are.na channel contents into the render for use w mustache.js by using `{{arena}}` - see views/arena.html
+        })
+      }
+    })
+    .catch(err => {
+      // handle errors
 
-			cache.details.title = 'Error 😭' // change the value of cache.details.title (loaded from ./api/config.yaml) to reflect an error
+      cache.details.title = 'Error 😭' // change the value of cache.details.title (loaded from ./api/config.yaml) to reflect an error
 
-			console.log(err)
-			res.render('index.html', {
-				title: 'Error 😭',
-				static_url: cdn,
-				config: cache
-			})
-		})
+      console.log(err)
+      res.render('index.html', {
+        title: 'Error 😭',
+        static_url: cdn,
+        config: cache
+      })
+    })
+})
+
+app.get('/writing', async function(req, res) {
+  const view = req.query.view
+  const arena = new Arena({ accessToken: process.env.arenaPAT })
+  arena
+    .channel(process.env.arenaChannel)
+    .get({
+      page: 1, // get the first page of results
+      per: 64, // get 64 items per call (max: 100) - play around w this for performance
+      direction: 'desc' // ask API v nicely to sort blocks by most recent
+    })
+    .then(channel => {
+      // fetch our whole are.na channel as `channel`
+
+      const config = yaml.safeLoad(channel.metadata.description) // get our site description from our are.na channel description - since it is loaded in as yaml, we can access it's values with `config.key`, ex: for title, we can use `config.details.title`
+      const contents = channel.contents // clean up the results a little bit, and make the channel's contents available as a constant, `contents`
+
+      const calendar = contents.pop() // pop last block (in this case, "calendar"), out of array, and then pass it to the render below
+
+      if (view === 'channel') {
+        // append `?view=channel` to the end of your URL to see "channel" as JSON
+        res.send(contents)
+      } else if (view === 'calendar') {
+        // append `?view=calendar` to the end of your URL to see "calendar" as JSON
+        res.send(calendar)
+      } else {
+        res.render('writing.html', {
+          static_url: cdn,
+          config,
+          calendar,
+          arena: contents // pass our are.na channel contents into the render for use w mustache.js by using `{{arena}}` - see views/arena.html
+        })
+      }
+    })
+    .catch(err => {
+      // handle errors
+
+      cache.details.title = 'Error 😭' // change the value of cache.details.title (loaded from ./api/config.yaml) to reflect an error
+
+      console.log(err)
+      res.render('writing.html', {
+        title: 'Error 😭',
+        static_url: cdn,
+        config: cache
+      })
+    })
 })
 
 app.use(function(req, res) {
-	res.status(404)
+  res.status(404)
 
-	if (req.accepts('html')) {
-		res.render('404', {
-			url: req.url,
-			static_url: '...',
-			title: 'Page not found'
-		})
-	} else {
-		res.send('404')
-	}
+  if (req.accepts('html')) {
+    res.render('404', {
+      url: req.url,
+      static_url: '...',
+      title: 'Page not found'
+    })
+  } else {
+    res.send('404')
+  }
 })
 
 if (environment !== 'now') {
-	const port = process.env.PORT || 3000
-	app.listen(port, function() {
-		console.log(`Up on port ${port} 🥚`)
-	})
+  const port = process.env.PORT || 3000
+  app.listen(port, function() {
+    console.log(`Up on port ${port} 🥚`)
+  })
 } else {
-	app.listen()
+  app.listen()
 }
 // what's happening here ?
 // `now` does not like it when ports are specified for it to use, or when options are set inside `app.listen(...)`. However, nodemon _does_ like having options set for ports, so this sets options based on what you are current running/building the server with
